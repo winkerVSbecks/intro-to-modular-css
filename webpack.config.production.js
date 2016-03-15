35e741c2ccadbd87b2573e67247fee3f4e333592
Varun Vachhar <varunvachhar@gmail.com>
2016-03-15 12:05:23 -0400

    🐛 build script

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -13,7 +13,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist/dist'),
     filename: '[name].js',
     publicPath: '/'
   },
