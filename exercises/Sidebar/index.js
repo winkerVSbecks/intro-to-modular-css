47889b075b11f12ff012f7690a5f838315ceaee8
Fatima Remtullah <fatima.remtullah@rangle.io>
2017-03-14 12:17:15 -0400

    update-sidebar

--- exercises/Sidebar/index.js
+++ exercises/Sidebar/index.js
@@ -5,31 +5,31 @@ import './Sidebar.css';
 const Sidebar = () => {
   return (
     <div>
-      <aside className="fixed top-0 bottom-0 left-0 col-3 bg-aqua Sidebar">
-        <h3 className="caps bold navy px2">Sidebar</h3>
-        <ul className="list-reset navy">
-          <li className="px2 py1">List Reset</li>
-          <li className="px2 py1">Removes bullets</li>
-          <li className="px2 py1">Removes numbers</li>
-          <li className="px2 py1">Removes padding</li>
+      <aside className="fixed top-0 bottom-0 left-0 w-25 bg-light-blue Sidebar">
+        <h3 className="ttu tracked bold navy ph3">Sidebar</h3>
+        <ul className="list pl0 navy">
+          <li className="ph3 pv2">List Reset</li>
+          <li className="ph3 pv2">Removes bullets</li>
+          <li className="ph3 pv2">Removes numbers</li>
+          <li className="ph3 pv2">Removes padding</li>
         </ul>
       </aside>
 
-      <main className="px2 Sidebar__body">
-        <h3 className="caps bold">Main Content Area</h3>
-        <img className="mx-auto" src="http://placehold.it/600x400" />
+      <main className="ph3 Sidebar__body">
+        <h3 className="ttu tracked bold">Main Content Area</h3>
+        <img src="http://placehold.it/600x400" />
         <p>Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank tongue pig drumstick capicola swine short loin ham hock kevin.</p>
 
-        <img className="mx-auto" src="http://placehold.it/600x400" />
+        <img src="http://placehold.it/600x400" />
         <p>Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank tongue pig drumstick capicola swine short loin ham hock kevin.</p>
 
-        <img className="mx-auto" src="http://placehold.it/600x400" />
+        <img src="http://placehold.it/600x400" />
         <p>Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank tongue pig drumstick capicola swine short loin ham hock kevin.</p>
 
-        <img className="mx-auto" src="http://placehold.it/600x400" />
+        <img src="http://placehold.it/600x400" />
         <p>Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank tongue pig drumstick capicola swine short loin ham hock kevin.</p>
 
-        <img className="mx-auto" src="http://placehold.it/600x400" />
+        <img src="http://placehold.it/600x400" />
         <p>Bacon ipsum dolor sit amet chuck prosciutto landjaeger ham hock filet mignon shoulder hamburger pig venison. Ham bacon corned beef, sausage kielbasa flank tongue pig drumstick capicola swine short loin ham hock kevin.</p>
       </main>
     </div>
